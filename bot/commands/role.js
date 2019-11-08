f71b79487f162668ee8dce8a0799ce86fe3d3235
Rekch <descale@live.fr>
2019-11-07 23:35:20 -0500

    Ajout attribution role texte
    Ajout de la commande pour attribuer des rôles par texte

---
+++ bot/commands/role.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'role',
+	description: 'Add a role to the user !',
+	execute(message) {
+		const args = message.content.split(' ');
+		var role = message.guild.roles.find( 'name' , args[1]);
+		if(!role){
+			return message.channel.send('Sorry I can\'t find this role.');
+		}
+		message.member.addRole(role.id);
+		return message.channel.send('Job done sir.');
+ },
+};
